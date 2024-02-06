a081204b7204a0277b2ea0dd736bb35f635de9e5
Dark Master <lovesong7zip@gmail.com>
2024-02-06 16:25:06 +0330

    Create Remove_DnsForwarder.ps1

---
+++ Networking/Dns_WindowsServer/Remove_DnsForwarder.ps1
@@ -0,0 +1,15 @@
+<#
+.SYNOPSIS
+	remove dns forwarder
+.DESCRIPTION
+    remove dns forwarder
+	PS> ./Dns_role.ps1
+
+.LINK
+	https://github.com/pakoti/Awesome_Sysadmin
+.NOTES
+	Author: Dark Master | License: CC0-1,0
+#>
+
+Remove-DnsServerForwarder 8.8.8.8
+
